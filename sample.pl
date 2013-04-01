0b64df70e858a5f04037b3cd90f437155d27fea7
umeyuki <umeyuki1326+github@gmail.com>
2013-04-02 08:56:03 +0900

    Ishiki::Calculatorメソッド追加

--- sample.pl
+++ sample.pl
@@ -1,11 +1,11 @@
 #perl!
 use Findbin;
 use lib "$FindBin::Bin/lib";
-use Ishiki;
+use Ishiki::Calculator;
 use YAML::Tiny::Color;
 use Config::Pit;
 use utf8;
-use Config::Pit;
+
 
 my $config = pit_get(
     "e.developer.yahoo.co.jp",
@@ -16,21 +16,19 @@ my $config = pit_get(
 );
 
 my $app_id = $config->{app_id};
-warn $app_id;
-my $ishiki = Ishiki->new(
+my $ishiki = Ishiki::Calculator->new(
     yahoo_appid => $app_id,
 );
 my $keywords = require 'etc/sample.pl';
 
-my $norns =  $ishiki->get_norn('こんにちは、私の名前はumeyukiです。趣味は登山とです。座右の名は感謝');
+my $result =  $ishiki->calc('こんにちは、私の名前はumeyukiです。趣味は登山とです。座右の名は感謝',$keywords);
 
-
-my $result = 0;
-for my $key ( @$norns  ) {
-    if ( my $match = $keywords->{$key}) {
-        print $keywords->{$key} . "\n";
-        $result += $keywords->{$key};
-    }
-}
+# my $result = 0;
+# for my $word ( @$words  ) {
+#     if ( my $match = $keywords->{$word}) {
+#         print $keywords->{$word} . "\n";
+#         $result += $keywords->{$word};
+#     }
+# }
 
 print "Your Ishiki is ${result}";
