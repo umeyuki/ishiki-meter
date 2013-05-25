e20776bee9e52213debfdf899b42238c740860ce
umeyuki <umeyuki1326+github@gmail.com>
2013-05-25 20:58:55 +0900

    use OAuth::Lite::Consumer

--- config.pl
+++ config.pl
@@ -3,7 +3,8 @@
     twitter => {
         consumer_key    => 'yXQW9wB4VBNVE4YNLgD9kw',
         consumer_secret => '4Ah8096VJH132XtBS1UjxDpu2Vr4tr44YAfsL1sKBs',
-        count => 20
+        count => 20,
+        callback_url  => 'http://localhost:5000/auth/auth_twitter'
     },
     facebook => {
         app_id    => '488341777892340',
