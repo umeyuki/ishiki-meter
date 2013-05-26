f728f3cba01f28eab8b1f3399285ce9b5ffe15f6
umeyuki <umeyuki1326+github@gmail.com>
2013-05-26 16:54:15 +0900

    意識高いワードを色付け

--- ishiki_meter.pl
+++ ishiki_meter.pl
@@ -21,23 +21,23 @@ my $config = plugin( 'Config' => { file => "config.pl" } );
 my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
     apiurl           => 'http://api.twitter.com/1.1',
     legacy_lists_api => 0,
-    consumer_key    => $config->{twitter}->{consumer_key},
-    consumer_secret => $config->{twitter}->{consumer_secret},
+    consumer_key     => $config->{twitter}->{consumer_key},
+    consumer_secret  => $config->{twitter}->{consumer_secret},
 );
 
 app->secret( $config->{secret} );
 
 helper redis => sub {
-    Redis->new(%{$config->{Redis}});
+    Redis->new( %{ $config->{Redis} } );
 };
 
 helper keywords => sub {
-    my ( $self ) = shift;
+    my ($self) = shift;
 
     #TODO use redis
-    my $keywords = {};# || $self->redis->get('KEYWORDS');;
+    my $keywords = {};    # || $self->redis->get('KEYWORDS');;
     if ( keys %$keywords <= 0 ) {
-        my $dbh = DBI->connect(@{$config->{DBI}});
+        my $dbh = DBI->connect( @{ $config->{DBI} } );
         $dbh->{unicode} = 1;
         my $sql = <<SQL;
 SELECT
@@ -47,29 +47,47 @@ FROM
 SQL
         my $sth = $dbh->prepare($sql);
         $sth->execute();
-        my $rows = $sth->fetchall_arrayref({});
-        for my $row ( @$rows ) {
-            $keywords->{$row->{name}} = $row->{value};
+        my $rows = $sth->fetchall_arrayref( {} );
+        for my $row (@$rows) {
+            $keywords->{ $row->{name} } = $row->{value};
         }
 
-#        $self->redis->rpush( 'KEYWORDS' , $_ ) for (@$keywords);
+        #        $self->redis->rpush( 'KEYWORDS' , $_ ) for (@$keywords);
         $sth->finish;
         $dbh->disconnect;
     }
-    
+
     $keywords;
 };
 
 helper ishiki => sub {
-    my ( $self ) = shift;
+    my $self = shift;
     my $app_id = $config->{yahoo}->{app_id};
 
     Ishiki::Parser->new( yahoo_appid => $app_id );
 };
 
+helper pickup_words => sub {
+    my ($self,$remarks) = @_;
+
+    my @result = ();
+    my @keywords = keys %{$self->{keywords}};
+
+    for my $remark ( @$remarks ) {
+        for my $keyword ( @keywords ) {
+            if ( $remark =~ /$keyword/ ){
+                my $font = $self->{keywords}->{$remark} * 20;
+                $remark =~ s|$keyword|<span style=\"font:20px;color:red\">$keyword</span>|;
+            }
+        }
+
+        push @result ,$remark;
+    }
+    return \@result;
+};
+
 =head2 twitter oauth
 
-set session twitter profile and recently 20 tweets
 
 =cut
 
@@ -83,8 +101,8 @@ sub startup {
 get '/auth/auth_twitter' => sub {
     my $self = shift;
 
-    my $session      = Plack::Session->new( $self->req->env );                                
-    
+    my $session = Plack::Session->new( $self->req->env );
+
     my $verifier = $self->req->param('oauth_verifier');
     my $consumer = OAuth::Lite::Consumer->new(
         consumer_key       => $config->{twitter}->{consumer_key},
@@ -94,44 +112,47 @@ get '/auth/auth_twitter' => sub {
         access_token_path  => q{/oauth/access_token},
         authorize_path     => q{/oauth/authorize},
     );
-    if (! $verifier) {
+    if ( not $verifier ) {
         my $request_token = $consumer->get_request_token(
-            callback_url => $config->{twitter}->{callback_url}
+            callback_url => $config->{twitter}->{callback_url} );
+        $session->set( request_token => $request_token );
+        $self->redirect_to(
+            $consumer->url_to_authorize(
+                token => $request_token
+            )
         );
-        $session->set( request_token => $request_token);
-        $self->redirect_to( $consumer->url_to_authorize(
-            token => $request_token
-        ) );
-    } else {
+    }
+    else {
         my $request_token = $session->get('request_token');
-        my $access_token = $consumer->get_access_token(
+        my $access_token  = $consumer->get_access_token(
             token    => $request_token,
             verifier => $verifier
         );
         $session->remove('request_token');
         my $credentials_res = $consumer->request(
             method => 'GET',
-            url    => q{http://api.twitter.com/1/account/verify_credentials.json},
-            token  => $access_token,
+            url => q{http://api.twitter.com/1/account/verify_credentials.json},
+            token => $access_token,
         );
-        my $user = JSON->new->utf8->decode($credentials_res->decoded_content);
+        my $user = JSON->new->utf8->decode( $credentials_res->decoded_content );
         my $tl_res = $consumer->request(
             method => 'GET',
             url    => 'https://api.twitter.com/1.1/statuses/user_timeline.json',
             token  => $access_token,
             params => { count => 10 }
         );
-        my $timeline = decode_json($tl_res->decoded_content);
-        my @tweets = ();
+        my $timeline = decode_json( $tl_res->decoded_content );
+        my @tweets   = ();
+
         for my $tweet ( @{$timeline} ) {
             push @tweets, $tweet->{text};
         }
-        
-        $session->set('screen_name' => $user->{name} );
-        $session->set('description' => $user->{description} );
-        $session->set( 'remarks'    => \@tweets );
+
+        $session->set( 'screen_name' => $user->{name} );
+        $session->set( 'profile'     => $user->{description} );
+        $session->set( 'remarks'     => \@tweets );
         $self->redirect_to('/');
-        
+
     }
 
 };
@@ -139,18 +160,25 @@ get '/auth/auth_twitter' => sub {
 get '/' => sub {
     my $self = shift;
 
-    my $session      = Plack::Session->new( $self->req->env );
-    my ($screen_name,$description,$remarks,$ishiki);
-    
-    if ($session->get('screen_name') ) {
+    $self->{keywords} = $self->keywords;    
+    my $session = Plack::Session->new( $self->req->env );
+
+    my ( $screen_name, $profile, $remarks, $ishiki );
+
+    if ( $session->get('screen_name') ) {
         $screen_name = $session->get('screen_name');
-        $description = $session->get('description');
+        $profile     = $session->get('profile');
         $remarks     = $session->get('remarks');
-        $ishiki      = $self->ishiki->calc($remarks,$self->keywords);
+        
+        my @data = ( $profile, @$remarks );
+        my $keywords = $self->{keywords};
+        warn "keyword!";
+        warn Dumper $keywords;
+        $ishiki = $self->ishiki->calc( \@data, $self->{keywords} );
     }
     $self->stash->{screen_name} = $screen_name;
-    $self->stash->{description} = $description;
-    $self->stash->{remarks}     = $remarks;
+    $self->stash->{profile}     = $profile;
+    $self->stash->{remarks}     = $self->pickup_words($remarks);
     $self->stash->{ishiki}      = $ishiki;
     $self->render('index');
 };
@@ -161,7 +189,6 @@ get '/logout' => sub {
     $session->expire();
     $self->redirect_to('/');
 };
-
 
 builder {
     enable "Plack::Middleware::AccessLog", format => "combined";
